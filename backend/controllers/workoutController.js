64d8a87f31584e32e958fd77cd60525c19654e63
dajs64 <dajs64@gmail.com>
2022-06-07 11:10:01 -0400

    full crud API

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,10 +1,15 @@
 const asyncHandler = require('express-async-handler')
+const { globalAgent } = require('http')
+
+const Workout = require('../models/workoutModel')
 
 //  Get workouts (hopefully specific users workouts)
 // Get /api/workouts
 // access Private 
 const getWorkouts = asyncHandler (async (req, res) => {
-  res.status(200).json({ message: 'Get workouts' })
+  const workouts = await Workout.find()
+
+  res.status(200).json(workouts)
 })
 
 // Set workout (hopefully specific users workouts)
@@ -16,21 +21,44 @@ const setWorkout = asyncHandler (async (req, res) => {
     throw new Error('Add a textfield')
 
   }
-  res.status(200).json({ message: 'Set workout' })
+
+  const workout = await Workout.create({
+  text: req.body.text
+})
+
+  res.status(200).json(workout)
 })
 
 // Update workout (hopefully specific users workouts)
 // PUT /api/workouts/:id
 // access Private 
 const updateWorkout = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update workout ${req.params.id}` })
-})
+  const workout = await Workout.findById(req.params.id)
+
+  if(!workout) {
+    res.status(400)
+    throw new Error ('Workout not found')
+  }
+
+  const updatedWorkout = await Workout.findByIdAndUpdate(req.params.id, req.body, {new: true,
+  })
+
+  res.status(200).json(updateWorkout)
 
 // Delete workout (hopefully specific users workouts)
 //  Delete /api/workouts/:id
 // access Private 
 const deleteWorkout = asyncHandler (async (req, res) => {
-  res.status(200).json({ message: `Delete workout ${req.params.id}` })
+  const workout = await Workout.findById(req.params.id)
+
+  if(!workout) {
+    res.status(400)
+    throw new Error ('Workout not found')
+  }
+
+  await globalAgent.remove()
+
+  res.status(200).json({ id: req.params.id })
 })
 
 
